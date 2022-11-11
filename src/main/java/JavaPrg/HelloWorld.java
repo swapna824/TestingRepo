42b671615f3f694d3f144a2fd12bd56b1ba0c79c
swapna824 <swapnagelima@gmail.com>
2022-11-11 16:30:03 +0530

    adding hello world.java prg

---
+++ src/main/java/JavaPrg/HelloWorld.java
@@ -0,0 +1,12 @@
+package JavaPrg;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+for(int i=1;i<=10;i++)
+	System.out.println(i);
+	}
+
+}
